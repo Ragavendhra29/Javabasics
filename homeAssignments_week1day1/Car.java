48b75e09f6a345b114ac3f3afa58def2c6732e9c
Ragavendhra29 <116647217+Ragavendhra29@users.noreply.github.com>
2022-10-26 14:27:29 +0530

    Add files via upload

---
+++ homeAssignments_week1day1/Car.java
@@ -0,0 +1,31 @@
+package homeAssignments_week1day1;
+
+public class Car {
+	public static void main(String[] args) {
+					
+			Car obj = new Car();
+			obj.isPuncture();
+			obj.driveCar();
+			obj.soudHorn();
+			obj.applyBrake();
+		
+	}
+	
+	public void driveCar() 
+	{
+	System.out.println("start engine to drive the car");
+	}
+	public void applyBrake() 
+	{
+	System.out.println("apply brake to stop the car");
+	}
+	public void soudHorn() 
+	{
+	System.out.println("press horn if there is any disturbance");
+	}
+	public void isPuncture() 
+	{
+	System.out.println("check car is puncture before starting the car");
+	}
+}
+	
